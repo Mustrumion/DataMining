a33bf9e59f7847013d5aa3ea066e2eeb9375f90d
Mustrumion <bar.popiel@gmail.com>
2017-11-21 13:13:31 +0100

    Proposed directory tree for data files

--- Main.py
+++ Main.py
@@ -2,3 +2,6 @@ from Reader import Reader
 
 reader = Reader()
 reader.LoadTestAds(2017,1);
+
+reader2 = Reader()
+reader.LoadQueries(2017,1,18);
--- Reader.py
+++ Reader.py
@@ -9,13 +9,19 @@ class Reader:
 				"private_business","has_person","photo_sizes","paidads_id_index","paidads_valid_to",\
 				"predict_sold","predict_replies","predict_views","reply_call","reply_sms","reply_chat",\
 				"reply_call_intent","reply_chat_intent")
+		self.DefaultQueriesUsedNames = ("phrase", "category_id", "sessions_count")
 		self.UsedNames = None
 		self.DataFrame = None
 		
-	def LoadByPath(self, path):
-		self.DataFrame = pd.read_csv(path, names = self.UsedNames, delimiter = ",", low_memory = False)
 
 	def LoadTestAds(self, year, month):
 		if self.UsedNames is None:
 			self.UsedNames = self.DefaultTestUsedNames
-		self.LoadByPath("AdsTraining\\ads_%d_%s_01\\001_anonimized" % (year, str(month).zfill(2)))
+		path = "AdsTraining\\ads_%d_%s_01\\001_anonimized" % (year, str(month).zfill(2))
+		pd.read_csv(path, names = self.UsedNames, delimiter = ",", low_memory = False)
+
+	def LoadQueries(self, year, month, day):
+		if self.UsedNames is None:
+			self.UsedNames = self.DefaultQueriesUsedNames
+		path = "Queries\\search_queries_%d_%s_%s.csv" % (year, str(month).zfill(2), str(day).zfill(2))
+		pd.read_csv(path, names = self.UsedNames, delimiter = ",", low_memory = False, header = None)
